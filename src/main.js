eb439ea4521da70f333929b79585be7cf27da0fc
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-10-12 14:16:15 +0400

    add routes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/routes";
+import store from "./store";
 
-Vue.config.productionTip = false
+import "./assets/scss/main.scss";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
