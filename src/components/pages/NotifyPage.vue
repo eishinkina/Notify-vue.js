4a65f76d23a54c530e33e23d7fab356a7b680614
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-10-12 17:38:38 +0400

    loading the content, installing the loader

--- src/components/pages/NotifyPage.vue
+++ src/components/pages/NotifyPage.vue
@@ -2,15 +2,17 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <!-- wrapper  -->
         <div class="notify__wrapper">
-          <!-- title  -->
+          <!-- title -->
           <div class="notify-title">
             <p>Notify App</p>
-            <!-- svg  -->
           </div>
-          <!-- notify  -->
-          <div class="notify__content">
+          <!-- loading spinner -->
+          <div v-if="loading" class="loading-section">
+            <div class="loading-spinner"></div>
+          </div>
+          <!-- notify -->
+          <div class="notify__content" v-if="!loading">
             <notify :messages="messages" />
           </div>
         </div>
@@ -32,8 +34,17 @@ export default {
       messages: [],
     };
   },
-  created() {
-    this.messages = api.notify;
+  mounted() {
+    this.getNotify();
+  },
+  methods: {
+    getNotify() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+        this.messages = api.notify;
+      }, 2000);
+    },
   },
 };
 </script>
@@ -45,28 +56,55 @@ export default {
   justify-content: center;
   height: 90vh;
 }
+
 .notify__wrapper {
   width: 400px;
   background-color: #fff;
   padding: 30px;
   border-radius: 16px;
   box-shadow: 0 12px 22px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
+
 .notify__content {
   display: flex;
   align-items: center;
   flex-direction: column;
   min-height: 300px;
 }
+
 .notify-title {
   display: flex;
   align-items: center;
   justify-content: center;
   margin-bottom: 30px;
+
   p {
     font-size: 24px;
     font-weight: 700;
     color: #000;
+  }
+}
+
+.loading-spinner {
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+  margin: auto;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
